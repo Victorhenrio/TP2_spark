2dcde22d47e7783da51fc965b463124607de12bf
Victor Henrio <pro.henrio@gmail.com>
2021-01-04 13:07:25 +0100

    alexandre work

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -8,11 +8,9 @@ import org.apache.commons.io.FilenameUtils
 import java.io._
 import spray.json._
 
-import fr.esme.gdpr.configuration.{ConfigReader, JsonConfig}
 import org.apache.spark.sql.functions.{col, udf}
 import java.text.SimpleDateFormat
 import java.util.Date
-import fr.esme.gdpr.utils.schemas.DataFrameSchema
 
 import scala.reflect.io.Path
 
@@ -36,10 +34,8 @@ object Launcher {
     val titles = new File("data/").listFiles.map(_.getName).toList
     titles.foreach{println}
 
-    titles.foreach{println}
-
     val schema = StructType(
-        StructField("amount", IntegerType, true) ::
+      StructField("amount", IntegerType, true) ::
         StructField("base_currency", StringType, true) ::
         StructField("currency", StringType, true) ::
         StructField("exchange_rate", DoubleType, true) ::
@@ -48,18 +44,34 @@ object Launcher {
     val dataRDD = sparkSession.sparkContext.emptyRDD[Row]
     var alldf = sparkSession.createDataFrame(dataRDD,schema)
 
+    var temp = alldf
+
     for (file <- titles ) {
-      val df:DataFrame = sparkSession.read.option("delimiter", ",").option("inferSchema", true).option("header", false).csv("data/"+file.toString)
+      val df: DataFrame = sparkSession.read.option("delimiter", ",").option("inferSchema", true).option("header", false).csv("data/" + file.toString)
       val filenamewithoutext = FilenameUtils.removeExtension(file)
+
       val input_format = new SimpleDateFormat("ddMMyyyy")
       val output_format = new SimpleDateFormat("dd-MM-yyyy")
+
       val formatted_Date = output_format.format(input_format.parse(filenamewithoutext))
-      val dfwithdate = df.withColumn("date",lit(formatted_Date))
+      var dfwithdate = df.withColumn("date", lit(formatted_Date))
       alldf = alldf.union(dfwithdate)
-      alldf.show()
-      println(alldf.schema)
+      //alldf.show()
+      //println(alldf.schema)
 
+      val date_count = alldf.filter(col(colName = "date") === formatted_Date)
+      val nb = date_count.count()
+      println(nb)
 
+      if (nb < 8) {
+        val df_limit = temp.limit(8 - nb.toInt)
+        val new_df = dfwithdate.union(df_limit)
+        new_df.show()
+        new_df.groupBy("date").count().show()
+
+      }
+
+      temp = dfwithdate
     }
 
     alldf.write.partitionBy("date").mode(SaveMode.Overwrite).parquet("result")
@@ -67,13 +79,11 @@ object Launcher {
     val json = sparkSession.read.json("conf/config.json")
     val jconf = json.collect()
     var day_ago = jconf(2)(0).toString
-    println(day_ago)
     day_ago = day_ago.replace("  \"fillWithDaysAgo\": ", "")
     val day_ago_int = day_ago.toInt
     println(day_ago_int)
 
     var file_day = jconf(1)(0).toString
-    println(file_day)
     file_day = file_day.replace("  \"date\": ", "").replace("\"","").replace(",","")
     println(file_day)
 
